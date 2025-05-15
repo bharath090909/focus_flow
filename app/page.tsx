654f71e78bbc0761693e445f1187647f01fc1873
Bharath <nallagondlabharath159@gmail.com>
2025-05-15 07:40:23 +0530

    Commeted some prisma code

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 import { PrismaClient } from "@prisma/client";
 
 export default async function Home() {
-  const client = new PrismaClient();
+  // const client = new PrismaClient();
 
-  const res = await client.users.create({
-    data: {
-      username: "Bharath",
-      password: "123",
-      age: 25,
-    },
-  });
+  // const res = await client.users.create({
+  //   data: {
+  //     username: "Jashu",
+  //     password: "123",
+  //     age: 25,
+  //   },
+  // });
 
-  console.log(res, "1234");
+  // console.log(res, "1234");
   return (
     <div className="">
       <h1>Hi There</h1>
